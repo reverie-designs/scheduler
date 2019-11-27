69d5df3411a3c4b9b96708d3e08339056ef33795
reverie-designs <dfrumovitch@gmail.com>
2019-11-26 20:20:36 -0500

    add componenet Form sun of Appointment

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+import InterviewerList from "../InterviewerList";
+import Button from "../Button";
+
+export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  
+  const reset = () => {
+    setInterviewer(null); 
+    setName("");
+  };
+
+  const save = () => {
+    props.onSave(name, interviewer);
+  }
+  
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={name}
+            onChange={event=>setName(event.target.value)}
+          />
+        </form>
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer}/>
+
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={props.onCancel} onClick={reset}>Cancel</Button>
+          <Button confirm onClick={props.onSave} onClick={save}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
