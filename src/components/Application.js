edf2fa2785cfee1459ae2a08b044de813acd13a7
reverie-designs <dfrumovitch@gmail.com>
2019-11-27 18:49:25 -0500

    add feature getInterview data with updated states from selected day's appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,122 +3,88 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-
-const appointments = [
-  {
-    id: 1,
-    time: "12:00pm",
-  },
-  {
-    id: 2,
-    time: "12:30pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 2.5,
-    time: "12:45pm"
-  },
-  {
-    id: 3,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png",
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "1:30pm",
-    interview: {
-      student: "Lee Walters",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "3pm",
-    interview: {
-      student: "Mark Jones",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/LpaYcom82x.png",
-      }
-    }
-  },
-];
-
-const printAppoints = appointments.map(appointment => {
-    return (
-      <Appointment key={appointment.id} {...appointment}
-      />
-    )
-}); 
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   
+
+  //manages all tracked states of the app
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
+
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days}));
-  
 
 
- 
   // Loads days from api once at page load
   useEffect(()=>{
-    axios.get("http://localhost:8000/api/days")
-      .then((response)=> 
-        setDays(response.data)); } , []);
-  
-        
-  // const getAppointmentsForDay = (state, day) => {
+    Promise.all([
+      axios.get("http://localhost:8000/api/days"), 
+      axios.get("http://localhost:8000/api/appointments"), 
+      axios.get("http://localhost:8000/api/interviewers"), 
+    ])
+    .then((all)=> {
+        setState(prev => ({days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+      });
+  }, []); //close useEffect
 
-  // };
+
+  //returns an array of current day's appointments  
+  const appointments =  getAppointmentsForDay(state, state.day);
+
+
+  //takes in the above array and returns a components for each appointment in the array     
+  const printAppoints = appointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+      return (
+        <Appointment 
+          key={appointment.id}
+          id={appointment.id}
+          time={appointment.time}
+          interview={interview}
+        />
+      )
+  }); 
+
+  let lastApp = <Appointment key="last" time="5pm" />;
+  printAppoints.push(lastApp);
 
   return (
     <main className="layout">
+
       <section className="sidebar">
+
         <img
           className="sidebar--centered"
           src="images/logo.png"
           alt="Interview Scheduler"
         />
+
         <hr className="sidebar__separator sidebar--centered" />
+
         <nav className="sidebar__menu">
-        <DayList
-          days={state.days}
-          day = {state.day}
-          setDay={setDay}
-        />
+            <DayList
+              days={state.days}
+              day = {state.day}
+              setDay={setDay}
+            />
         </nav>
+
         <img className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
-          alt="Lighthouse Labs"/>
+          alt="Lighthouse Labs"
+        />
+
       </section>
+
       <section className="schedule">
        {printAppoints}
-       <Appointment key="last" time="5pm" />
       </section>
+
     </main>
   );
 }
