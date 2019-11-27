aa78bbdbecf020d66f6e59ba8e8f70151c885a57
reverie-designs <dfrumovitch@gmail.com>
2019-11-26 20:21:45 -0500

    import head and show and empty to appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
+
   return (
   <article className="appointment">
-    {props.time}
+    <Header time={props.time}/>
+    {props.interview ? <Show interviewer={props.interview.interviewer} student={props.interview.student} />: <Empty />}
   </article>
   );
 }
