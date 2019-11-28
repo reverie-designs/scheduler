4d55a92b718bea79584796fd3c58d3e5550e8ed4
reverie-designs <dfrumovitch@gmail.com>
2019-11-28 17:06:36 -0500

    fix bug reseting day state

--- src/components/Application.js
+++ src/components/Application.js
@@ -13,7 +13,6 @@ export default function Application(props) {
     day: "Monday",
     days: [],
     appointments: {},
-    interviewers: {}
   });
 
 
@@ -28,7 +27,7 @@ export default function Application(props) {
       axios.get("http://localhost:8000/api/interviewers"), 
     ])
     .then((all)=> {
-        setState(prev => ({days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+        setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
       });
   }, []); //close useEffect
 
@@ -46,6 +45,7 @@ export default function Application(props) {
           id={appointment.id}
           time={appointment.time}
           interview={interview}
+          interviewers = {state.interviewers}
         />
       )
   }); 
