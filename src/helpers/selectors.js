a4b771b3ba33b6c4e1ee348d84d6df8cc36454ee
reverie-designs <dfrumovitch@gmail.com>
2019-11-28 18:35:50 -0500

    add helper function getInterviewersForDay

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -11,6 +11,8 @@ export function getAppointmentsForDay(state, day) {
       let thisDayApps = filteredAppointments[0].appointments;
       const results =[];
       thisDayApps.forEach(app => results.push(state.appointments[`${app}`]))
+
+      // console.log('APPOINTMENST',results);
       return results;
     }
 };
@@ -29,4 +31,20 @@ export function getInterview(state, interview) {
   } else {
       return null;
   }
+};
+
+
+export function getInterviewersForDay(state, day) {
+    
+  //reuturns on object for that day
+    const filteredInterviewers = state.days.filter(specDay => specDay.name === day);
+
+    if (filteredInterviewers.length === 0 || state.days === undefined || state.days.length === 0 || state.interviewers.length === 0 || !filteredInterviewers[0].interviewers){
+      return [];
+    } else {
+      let thisDayInterviewers = filteredInterviewers[0].interviewers;
+      const results =[];
+      thisDayInterviewers.forEach(interv => results.push(state.interviewers[`${interv}`]))
+      return results;
+    }
 };
