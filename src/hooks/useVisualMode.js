1d07ed14a064e359bf18dc79f443c7a9280bcfd2
reverie-designs <dfrumovitch@gmail.com>
2019-12-02 14:30:37 -0500

    fix bug not a function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,26 +1,26 @@
 // eslint-disable-next-line 
-import React, {useState} from "react"
+import React, {useState} from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial)
-  const [history, setHistory] = useState([initial])
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   function transition (newMode, replace = false){
     if(!replace){
-      setMode(newMode)
-      setHistory(history=> [mode, ...history])
+      setMode(newMode);
+      setHistory(history=> [mode, ...history]);
     }
-    setMode(newMode)
-  }
+    setMode(newMode);
+  };
 
   function back (){
     if (history.length === 1) {
-      setHistory(initial)
-      setMode(mode)
+      setHistory(initial);
+      setMode(mode);
     } else {
-      setHistory(([ _ ,...history])=> history)
-      setMode(history[0])
+      setHistory(([ _ ,...history])=> history);
+      setMode(history[0]);
     }   
-  }
+  };
   return {mode, transition, back}
 }
