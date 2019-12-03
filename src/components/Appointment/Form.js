d8dea0319eb23165c14dea3ad428a82f2f86a948
reverie-designs <dfrumovitch@gmail.com>
2019-12-02 21:28:22 -0500

    show error message if trying to save form with empty field

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -11,6 +11,18 @@ export default function Form(props) {
     setName(""))
   }
 
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError("");
+    props.onSave(name, interviewer);
+  }
+
+  const [error, setError] = useState("");
+
   return (
     <main className="appointment__card appointment__card--create">
 
@@ -19,6 +31,7 @@ export default function Form(props) {
         <form autoComplete="off" onSubmit={event => event.preventDefault()}>
 
           <input
+            data-testid="student-name-input"
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
@@ -26,9 +39,9 @@ export default function Form(props) {
             value={name}
             onChange={event=>setName(event.target.value)}
           />
+          <section className="appointment__validation">{error}</section>
           
         </form>
-
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer}/>
 
       </section>
@@ -38,7 +51,8 @@ export default function Form(props) {
         <section className="appointment__actions">
 
           <Button  danger onClick={reset}>Cancel</Button>
-          <Button  confirm onClick={()=>props.onSave(name, interviewer)}>Save</Button>
+          <Button  confirm onClick={validate}>Save</Button>
+          {/* <Button  confirm onClick={()=>props.onSave(name, interviewer)}>Save</Button> */}
 
         </section>
 
